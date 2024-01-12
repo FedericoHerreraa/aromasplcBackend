1faf9c230a96a9e01433993576d87041d6e995ef
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-12 13:37:38 -0300

    ip cambiada

--- src/controllers/prod.controller.js
+++ src/controllers/prod.controller.js
@@ -3,7 +3,6 @@ import Prod from '../schemas/prod.schema.js'
 export const getProducts = async (req, res) => {
     try {
         const response = await Prod.find({})
-        console.log(response)
         res.json(response)
     } catch (error) {
         res.status(404).json({ error: "No se pudieron encontrar las tareas" })
